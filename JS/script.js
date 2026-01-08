eaccd12abfef2c36c997b3ba504bf5a10e34f0be
Shubham soni <shubhamsoni98766@gmai.com>
2026-01-08 23:23:40 +0530

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+
+function showMessage() {
+    alert("We are in external file");
+}
+
+function openPdf(){
+    window.open("../Assets/Shubham_Soni_Resume.pdf", "_blank");
+
+    
+}
+function openLink(url){
+    const newTab  = window.open(url,'_blank');
+}
